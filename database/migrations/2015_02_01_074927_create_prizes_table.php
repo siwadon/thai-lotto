510c3a770bd650aaef52a7efa0bbd97312dc1f27
Siwadon Saosoong <siwadon.s@gmail.com>
2015-02-01 17:32:26 +0700

    Create lottery prizes table

---
+++ database/migrations/2015_02_01_074927_create_prizes_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePrizesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('prizes', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->date('date');
+			$table->string('type');
+			$table->string('prize');
+			$table->unique('id');
+			$table->unique( array('id', 'date', 'type', 'prize') );
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('prizes');
+	}
+
+}
