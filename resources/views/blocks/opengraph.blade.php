3ea11529a01938d4c3d3f96220a294d25c4510e2
Siwadon Saosoong <siwadon.s@gmail.com>
2015-02-18 23:49:28 +0700

    Add Facebook app ID

--- resources/views/blocks/opengraph.blade.php
+++ resources/views/blocks/opengraph.blade.php
@@ -3,3 +3,4 @@
 <meta property="og:url" content="{{ Request::url() }}" />
 <meta property="og:image" content="{{ asset('image/pungyen-facebook_post.jpg') }}" />
 <meta property="og:description" content="เลขที่ออก {{ $prizes['prizes']['first'][0] }} - {{ implode(', ', $prizes['prizes']['last_three_digits']) }} - {{ $prizes['prizes']['last_two_digits'][0] }}" />
+<meta property="fb:id" content="442768559205838" />
