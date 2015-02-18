377d4695de851753bbf2a610185186e5144e5183
Siwadon Saosoong <dew@abctech-thailand.com>
2015-02-18 19:36:38 +0700

    Add more lotter result in opengraph tag

--- resources/views/blocks/opengraph.blade.php
+++ resources/views/blocks/opengraph.blade.php
@@ -2,4 +2,4 @@
 <meta property="og:type" content="website" />
 <meta property="og:url" content="{{ Request::url() }}" />
 <meta property="og:image" content="" />
-<meta property="og:description" content="เลขที่ออก {{ $prizes['prizes']['first'][0]  }}" />
+<meta property="og:description" content="เลขที่ออก {{ $prizes['prizes']['first'][0] }}, {{ $prizes['prizes']['last_two_digits'][0] }}, {{ implode(', ', $prizes['prizes']['last_three_digits']) }}" />
