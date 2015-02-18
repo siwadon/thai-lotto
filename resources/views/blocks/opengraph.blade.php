fee63dad537a020264f731014171b5a071c4aa1f
Siwadon Saosoong <siwadon.s@gmail.com>
2015-02-18 23:42:09 +0700

    Add default image for opengraph

--- resources/views/blocks/opengraph.blade.php
+++ resources/views/blocks/opengraph.blade.php
@@ -1,5 +1,5 @@
 <meta property="og:title" content="ตรวจหวย วันที่ {{ $prizes['date']['human'] }} | ปังเย็น" />
 <meta property="og:type" content="website" />
 <meta property="og:url" content="{{ Request::url() }}" />
-<meta property="og:image" content="" />
+<meta property="og:image" content="{{ asset('image/pungyen-facebook_post.jpg') }}" />
 <meta property="og:description" content="เลขที่ออก {{ $prizes['prizes']['first'][0] }}, {{ $prizes['prizes']['last_two_digits'][0] }}, {{ implode(', ', $prizes['prizes']['last_three_digits']) }}" />
