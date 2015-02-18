3fc06a602ea8c99064c08836366b8eaef8dae727
Siwadon Saosoong <siwadon.s@gmail.com>
2015-02-18 23:47:47 +0700

    Change prize order in opengraph

--- resources/views/blocks/opengraph.blade.php
+++ resources/views/blocks/opengraph.blade.php
@@ -2,4 +2,4 @@
 <meta property="og:type" content="website" />
 <meta property="og:url" content="{{ Request::url() }}" />
 <meta property="og:image" content="{{ asset('image/pungyen-facebook_post.jpg') }}" />
-<meta property="og:description" content="เลขที่ออก {{ $prizes['prizes']['first'][0] }}, {{ $prizes['prizes']['last_two_digits'][0] }}, {{ implode(', ', $prizes['prizes']['last_three_digits']) }}" />
+<meta property="og:description" content="เลขที่ออก {{ $prizes['prizes']['first'][0] }} - {{ implode(', ', $prizes['prizes']['last_three_digits']) }} - {{ $prizes['prizes']['last_two_digits'][0] }}" />
