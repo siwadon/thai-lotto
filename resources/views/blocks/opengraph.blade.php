46c12beebf18690be6a73efe6f6f8342b6e54507
Siwadon Saosoong <dew@abctech-thailand.com>
2015-02-18 11:35:22 +0700

    Move google analytics and opengraph to subview

---
+++ resources/views/blocks/opengraph.blade.php
@@ -0,0 +1,5 @@
+<meta property="og:title" content="ตรวจหวย | ปังเย็น" />
+<meta property="og:type" content="website" />
+<meta property="og:url" content="{{ Request::url() }}" />
+<meta property="og:image" content="" />
+<meta property="og:description" content="เลขที่ออก {{ $prizes['prizes']['first'][0]  }}" />
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -2,14 +2,11 @@
 <head>
     <title ng-bind="'ตรวจหวย วันที่ ' + (result_date | date) + ' | Pungyen.com'">Lottery</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta property="og:title" content="ตรวจหวย | ปังเย็น" />
-    <meta property="og:type" content="website" />
-    <meta property="og:url" content="{{ Request::url() }}" />
-    <meta property="og:image" content="" />
-    <meta property="og:description" content="เลขที่ออก {{ $prizes['prizes']['first'][0]  }}" />
+    @include('blocks/opengraph', ['prizes' => $prizes])
     <link rel="stylesheet" href="//ajax.googleapis.com/ajax/libs/angular_material/0.7.1/angular-material.min.css">
     <link rel="stylesheet" href="{{ asset('css/lotto.css') }}">
     <base href="/">
+    @include('blocks/google_analytics')
 </head>
 <body>
     <div class="container" ng-controller="LottoController" layout="column" flex layout-fill>
@@ -20,18 +17,8 @@
     <script src="//ajax.googleapis.com/ajax/libs/angularjs/1.3.10/angular.min.js"></script>
     <script src="//ajax.googleapis.com/ajax/libs/angularjs/1.3.10/angular-animate.min.js"></script>
     <script src="//ajax.googleapis.com/ajax/libs/angularjs/1.3.10/angular-aria.min.js"></script>
-    <script src="/js/lotto.js"></script>
     <script src="//ajax.googleapis.com/ajax/libs/angular_material/0.7.1/angular-material.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/angular-i18n/1.2.15/angular-locale_th-th.js"></script>
-    <script>
-        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-        ga('create', 'UA-59835068-1', 'auto');
-        ga('send', 'pageview');
-
-    </script>
+    <script src="/js/lotto.js"></script>
 </body>
 </html>
