130ade3e8fd7a769dbec946261f68c806e44c52d
Siwadon Saosoong <siwadon.s@gmail.com>
2015-02-22 23:13:23 +0700

    Make the date menu dropdown works properly. It will update lottery result on date changed

--- public/js/lotto.js
+++ public/js/lotto.js
@@ -1,14 +1,26 @@
 Polymer('app-element', {
     ready: function() {
-        this.result_date = window.initialData.date;
-        this.prizes = [
-        { label: 'รางวัลที่หนึ่ง', data: window.initialData.prizes.first },
-        { label: 'เลขท้ายสองตัว', data: window.initialData.prizes.last_two_digits },
-        { label: 'เลขท้ายสามตัว', data: window.initialData.prizes.last_three_digits },
-        { label: 'รางวัลที่สอง',  data: window.initialData.prizes.second },
-        { label: 'รางวัลที่สาม',  data: window.initialData.prizes.third },
-        { label: 'รางวัลที่สี่',   data: window.initialData.prizes.fourth },
-        { label: 'รางวัลที่ห้า',   data: window.initialData.prizes.fifth },
+        this.setResult(window.initialData);
+    },
+    setResult: function(result) {
+        this.selected_date = result.date;
+        this.prizes = this.mapPrizes(result.prizes);
+    },
+    mapPrizes: function(prizes) {
+        return [
+            { label: 'รางวัลที่หนึ่ง', data: prizes.first },
+            { label: 'เลขท้ายสองตัว', data: prizes.last_two_digits },
+            { label: 'เลขท้ายสามตัว', data: prizes.last_three_digits },
+            { label: 'รางวัลที่สอง',  data: prizes.second },
+            { label: 'รางวัลที่สาม',  data: prizes.third },
+            { label: 'รางวัลที่สี่',   data: prizes.fourth },
+            { label: 'รางวัลที่ห้า',   data: prizes.fifth },
         ];
-    }
+    },
+    updateSelectedUrl: function(e, detail, sender) {
+        this.selected_url = '/api/result/' + e.target.templateInstance.model.date;
+    },
+    updateResult: function(e, detail, sender) {
+        this.setResult(detail.response);
+    },
 });
