0602a37a8ddd2df7be974a4fb30049cec5bdfc4b
Siwadon Saosoong <siwadon.s@gmail.com>
2015-08-10 21:13:50 +0700

    Use sitemap to indicate alternate language pages
    read more: https://support.google.com/webmasters/answer/2620865

--- resources/views/sitemap_xml.blade.php
+++ resources/views/sitemap_xml.blade.php
@@ -2,8 +2,18 @@
 
 @section('content')
   @foreach ($urls as $url)
+    <?php
+      $lang           = preg_match('/\/th\//', $url) ? 'th' : 'en';
+      $alternate_lang = $lang == 'th' ? 'en' : 'th';
+      $alternate_url  = str_replace("/$lang/", "/$alternate_lang/", $url);
+    ?>
     <url>
       <loc>{{ $url }}</loc> 
+      <xhtml:link
+          rel="alternate"
+          hreflang="{{ $alternate_lang }}"
+          href="{{ $alternate_url }}"
+      />
     </url>
   @endforeach
 @stop
